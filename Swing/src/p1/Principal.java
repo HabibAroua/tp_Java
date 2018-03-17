31fe1d0407553da93bc967d52b5a3bc9fed138e4
HabibAroua <habibha.aroua82@gmail.com>
2018-03-17 19:07:59 +0100

    Swing dev

---
+++ Swing/src/p1/Principal.java
@@ -0,0 +1,17 @@
+package p1;
+
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		JFrame fenetre = new JFrame();
+	    fenetre.setVisible(true);
+
+	}
+
+}
