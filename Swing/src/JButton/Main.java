aeb5971f7880f1d02466cc613b46f671bb9a69b0
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-03-23 21:26:51 +0100

    JButton explication

---
+++ Swing/src/JButton/Main.java
@@ -0,0 +1,18 @@
+package JButton;
+import javax.swing.*;
+//JButton()
+//JButton(String s)
+//JButton(Icon i)
+//public void setText(String s)
+//public void getEnabled(boolean b)
+//public void setIcon(Icon i)
+//public void addActionListener(ActionListener a)
+//public Icon getIcon()
+//public String getText()
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+	}
+}
