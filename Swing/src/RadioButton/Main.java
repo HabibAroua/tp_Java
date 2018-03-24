5dca443c96032b09f9aa7e5d185016b0b25dc744
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-03-24 20:59:50 +0100

    Add JRadioButton

---
+++ Swing/src/RadioButton/Main.java
@@ -0,0 +1,12 @@
+package RadioButton;
+//JRadioButton()
+//JRadioButton(String s)
+//JRadioButton(String s , boolean b)
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+	}
+
+}
