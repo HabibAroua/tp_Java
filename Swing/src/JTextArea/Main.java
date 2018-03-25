9348a09063178f2390bfde4506560b7ae869e70e
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-03-25 21:58:50 +0100

    JTextArea

---
+++ Swing/src/JTextArea/Main.java
@@ -0,0 +1,27 @@
+package JTextArea;
+import java.awt.Color;
+
+import javax.swing.*;
+//JTextArea()
+//JTextArea(String s)
+//JTextArea(int row , int column)
+//JTextArea(String s ,int row , int column)
+//public void setRows(int rows)
+//public void setColumns(int cols)
+//public void setFont(Font f)
+//public void insert(String s)
+//public void append(String s)
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		JTextArea area;
+		JFrame frame;
+		frame=new JFrame();
+		area=new JTextArea(200,300);
+		area.setBounds(15, 50, 200, 200);
+		area.setBackground(Color.darkGray);
+		area.setForeground(Color.ORANGE);
+	}
+}
