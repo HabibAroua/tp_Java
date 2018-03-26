56e63dff4adccf42ae0513c966173ff777987b7a
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-03-26 20:55:57 +0100

    Show the ComboBox

--- Swing/src/JComboBox/Main.java
+++ Swing/src/JComboBox/Main.java
@@ -1,8 +1,23 @@
 package JComboBox;
-
+import javax.swing.*;
 public class Main 
 {
+	JFrame frame;
+	JComboBox c;
+	Main()
+	{
+		frame=new JFrame("ComboBox");
+		String name[]={"Habib","Salah","Nada"};
+		c=new JComboBox(name);
+		c.setBounds(60,60,100,30);
+		frame.setSize(500, 500);
+		frame.add(c);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+	
 	public static void main(String[] args)
 	{
+		new Main();
 	}
 }
