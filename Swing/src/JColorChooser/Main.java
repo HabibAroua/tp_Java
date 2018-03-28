f9289ec34372d6f4e9149e5909dd9eeb6bc5accf
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-03-28 23:52:10 +0100

    Color

--- Swing/src/JColorChooser/Main.java
+++ Swing/src/JColorChooser/Main.java
@@ -1,10 +1,41 @@
 package JColorChooser;
-public class Main
+import javax.swing.JFrame;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JColorChooser;
+import javax.swing.JButton;
+
+
+public class Main extends JFrame implements ActionListener
 {
+	JButton btn;
+	Container con;
+	Main()
+	{
+		con=getContentPane();
+		btn.setLayout(new FlowLayout());
+		btn=new JButton("Colors");
+		btn.addActionListener(this);
+		
+		
+	}
+	
 	public static void main(String[] args)
 	{
-          //please write a code you're late
+		new Main();
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		Color initialColor =Color.BLUE;
+		Color colorC=JColorChooser.showDialog(this, "choose a color", initialColor);
+		con.setBackground(colorC);
+		
 	}
 
 }
