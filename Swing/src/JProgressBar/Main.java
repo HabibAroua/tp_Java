f098a5fda76f6f037093d8dc0a8fc9023987e718
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-03-30 22:20:44 +0100

    Show the JProgress Bar

--- Swing/src/JProgressBar/Main.java
+++ Swing/src/JProgressBar/Main.java
@@ -1,4 +1,6 @@
 package JProgressBar;
+import javax.swing.JProgressBar;
+import javax.swing.JFrame;
 //JProgressBar()
 //JProgressBar(int min , int max)
 //JProgressBar(int orient)
@@ -8,9 +10,24 @@ package JProgressBar;
 //public void setString(String s)
 //public void setOrientation(int orientation)
 //public void setValue(int value)
-public class Main 
+public class Main extends JFrame
 {
+	JProgressBar jProgressBar;
+	int i=0;
+	int num=0;
+	Main()
+	{
+		jProgressBar=new JProgressBar(0,1000);
+		jProgressBar.setBounds(30, 30, 300, 40);
+		jProgressBar.setValue(0);
+		jProgressBar.setStringPainted(true);
+		add(jProgressBar);
+		setSize(500,400);
+		setLayout(null);
+		setVisible(true);
+	}
 	public static void main(String[] args) 
 	{
+		new Main();
 	}
 }
