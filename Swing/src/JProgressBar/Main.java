690371535183bff8a1e5fbf4ea09f67f3b0eaaed
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-03-30 22:09:28 +0100

    JProgressBar method

---
+++ Swing/src/JProgressBar/Main.java
@@ -0,0 +1,16 @@
+package JProgressBar;
+//JProgressBar()
+//JProgressBar(int min , int max)
+//JProgressBar(int orient)
+//JProgressBar(int orient , int min , int max)
+
+//public void setStringPainted(boolean b)
+//public void setString(String s)
+//public void setOrientation(int orientation)
+//public void setValue(int value)
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+	}
+}
