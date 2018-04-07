95da2217c076c3b57e8fc81f24d7997d4595a980
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-04-07 19:26:24 +0100

    Show the Slider

--- Swing/src/jSlider/Main.java
+++ Swing/src/jSlider/Main.java
@@ -1,10 +1,25 @@
 package jSlider;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
 
-public class Main
+public class Main extends JFrame
 {
+	JSlider slider;
+	JPanel panel;
+	Main()
+	{
+		slider=new JSlider(JSlider.HORIZONTAL,0,70,25);
+	    panel=new JPanel();
+	    panel.add(slider);
+	    add(panel);
+	}
+	
 	public static void main(String[] args) 
 	{
-		
+		Main m=new Main();
+		m.pack();
+		m.setVisible(true);
 	}
 
 }
