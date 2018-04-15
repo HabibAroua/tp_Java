958a4be9e1fde2e5c8d80a2126fa620e2afcf0af
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-04-15 18:38:26 +0100

    Dev class LettreRecommandeAccuse

--- Heritage/src/Ex2Tp/LettreRecommandeAccuse.java
+++ Heritage/src/Ex2Tp/LettreRecommandeAccuse.java
@@ -1,5 +1,35 @@
 package Ex2Tp;
 
-public class LettreRecommandeAccuse {
+public class LettreRecommandeAccuse extends Lettre
+{
+	private String adresseRetour;
+	private float montantRelative;
+	
+	public LettreRecommandeAccuse()
+	{
+		super();
+	}
+	
+	public LettreRecommandeAccuse(String adresseDestinatire, String adresseExpediteur, float poids, float affranchissement , String adresseRetour , float montantRelative)
+	{
+		super(adresseDestinatire,adresseExpediteur,poids,affranchissement);
+		this.adresseRetour=adresseRetour;
+		this.montantRelative=montantRelative;
+	}
+	
+	public String getAdresseRetour()
+	{
+		return this.adresseRetour;
+	}
+	
+	public float getMontantRelative()
+	{
+		return this.montantRelative;
+	}
+	
+	public String toString()
+	{
+		return super.toString()+" le montant est "+getMontantRelative()+" l'adresse de retour est "+getAdresseRetour();
+	}
 
 }
