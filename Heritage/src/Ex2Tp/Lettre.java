8eb76649119355160ac54602757703a37d182d7f
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-04-15 13:22:44 +0100

    Dev class Lettre

--- Heritage/src/Ex2Tp/Lettre.java
+++ Heritage/src/Ex2Tp/Lettre.java
@@ -2,5 +2,65 @@ package Ex2Tp;
 
 public class Lettre 
 {
+	private String adresseDestinatire;
+	private String adresseExpediteur;
+	private float poids;
+	private float affranchissement;
+	
+	public Lettre()
+	{
+	}
 
+	public Lettre(String adresseDestinatire, String adresseExpediteur, float poids, float affranchissement) 
+	{
+		this.adresseDestinatire = adresseDestinatire;
+		this.adresseExpediteur = adresseExpediteur;
+		this.poids = poids;
+		this.affranchissement = affranchissement;
+	}
+
+	public String getAdresseDestinatire() 
+	{
+		return this.adresseDestinatire;
+	}
+
+	public void setAdresseDestinatire(String adresseDestinatire)
+	{
+		this.adresseDestinatire = adresseDestinatire;
+	}
+
+	public String getAdresseExpediteur() 
+	{
+		return this.adresseExpediteur;
+	}
+
+	public void setAdresseExpediteur(String adresseExpediteur)
+	{
+		this.adresseExpediteur = adresseExpediteur;
+	}
+
+	public float getPoids() 
+	{
+		return this.poids;
+	}
+
+	public void setPoids(float poids)
+	{
+		this.poids = poids;
+	}
+
+	public float getAffranchissement() 
+	{
+		return this.affranchissement;
+	}
+
+	public void setAffranchissement(float affranchissement)
+	{
+		this.affranchissement = affranchissement;
+	}
+	
+	public String toString()
+	{
+		return "adresse destinatire est "+getAdresseDestinatire()+" adresse expéditeur est "+getAdresseExpediteur()+" le poids en gamme "+getPoids()+" l'affranchissement est "+getAffranchissement();
+	}
 }
