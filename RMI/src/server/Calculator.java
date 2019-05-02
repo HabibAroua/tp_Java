a7131f60eaef5a346c11e98b689f8ed8d72224df
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-05-02 20:44:00 +0100

    Update Calculator.java

--- RMI/src/server/Calculator.java
+++ RMI/src/server/Calculator.java
@@ -8,5 +8,48 @@ public class Calculator implements InterfaceCalculator
 	{
 		
 	}
+	
+	private int sum(int x , int y)
+	{
+		return x+y;
+	}
+	
+	private int substraction(int x , int y)
+	{
+		return x-y;
+	}
+	
+	private int div(int x , int y)
+	{
+		try
+		{
+			return x/y;
+		}
+		catch(ArithmeticException ex)
+		{
+			System.out.println("Exception "+ex.getMessage());
+			return -1;
+		}
+	}
+	
+	private long multiplication(int x , int y)
+	{
+		return x*y;
+	}
+	
+	private long factorial(int x)
+	{
+		long aux=0;
+		for(int i=2; i<=x ;i++)
+		{
+			aux*=i;
+		}
+		return aux;
+	}
+	
+	private long power(int a , int b)
+	{
+		return (long) Math.pow(a, b);
+	}
 
 }
