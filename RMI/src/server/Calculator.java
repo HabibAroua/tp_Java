2d03019391b4a0db3519a5377765bba9cd70ba2c
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-05-02 21:01:12 +0100

    Update Calculator.java

--- RMI/src/server/Calculator.java
+++ RMI/src/server/Calculator.java
@@ -1,12 +1,35 @@
 package server;
 
+import java.util.Scanner;
 public class Calculator implements InterfaceCalculator
 {
 
 	@Override
 	public void Calculate() 
 	{
-		
+		int choice=0;
+		Scanner scannerChoice=new Scanner(System.in);
+		do
+		{
+			do
+			{
+				System.out.println("_________________________Menu___________________________");
+				System.out.println("1)Sum___________________________________________________");
+				System.out.println("2)Substraction__________________________________________");
+				System.out.println("3)Division______________________________________________");
+				System.out.println("4)Multiplication________________________________________");
+				System.out.println("5)Factorial_____________________________________________");
+				System.out.println("6)Power_________________________________________________");
+				System.out.println("7)Exit__________________________________________________");
+				System.out.println("________________________________________________________");
+				System.out.println("Please enter your choice");
+				choice =scannerChoice.nextInt();
+				switch(choice)
+				{
+				
+				}
+			}while((choice<=0) && (choice >7));
+		}while(choice != 7);
 	}
 	
 	private int sum(int x , int y)
