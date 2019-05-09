f887e018e1da876a6fc0583202fc0163048e4a9f
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-05-09 16:57:44 +0100

    Update ConnectionFactory.java

--- WebService/src/client/ConnectionFactory.java
+++ WebService/src/client/ConnectionFactory.java
@@ -1,5 +1,21 @@
 package client;
 
-public class ConnectionFactory {
-
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+public class ConnectionFactory
+{
+	private double API_VERSION = 0;
+	private String API = "";
+	
+	private String METHOD = "POST";
+	private String TYPE = "application/x-www-form-urlencoded";
+	private String USER_AGENT = "Mozilla/5.0";
+	private String data = "";
+	private URL connection;
+	private HttpURLConnection finalConnection;
+	
+	private HashMap<String,String> fields =new HashMap<String , String>();
+	
+	
 }
