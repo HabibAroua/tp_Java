dde89ba4afee707c34dadcb71134208bd6b17bf6
Habib Aroua <habib.aroua@sesame.com.tn>
2020-11-27 20:10:17 +0100

    Update Client.java

--- RMI/src/client/Client.java
+++ RMI/src/client/Client.java
@@ -24,50 +24,50 @@ public class Client
 	public static void main(String args[])
 	{
 		try 
-        {
-            Registry registry = LocateRegistry.getRegistry(1099);
-            InterfaceCalculator stub = (InterfaceCalculator) registry.lookup("InterfaceCalculator");
-            int choice=0;
-    		Scanner scannerChoice=new Scanner(System.in);
-    		do
-    		{
+        	{
+            		Registry registry = LocateRegistry.getRegistry(1099);
+            		InterfaceCalculator stub = (InterfaceCalculator) registry.lookup("InterfaceCalculator");
+            		int choice=0;
+    			Scanner scannerChoice=new Scanner(System.in);
     			do
     			{
-    				System.out.println("_________________________Menu___________________________");
-    				System.out.println("1)Sum___________________________________________________");
-    				System.out.println("2)Substraction__________________________________________");
-    				System.out.println("3)Division______________________________________________");
-    				System.out.println("4)Multiplication________________________________________");
-    				System.out.println("5)Factorial_____________________________________________");
-    				System.out.println("6)Power_________________________________________________");
-    				System.out.println("7)Exit__________________________________________________");
-    				System.out.println("________________________________________________________");
-    				System.out.println("Please enter your choice");
-    				choice =scannerChoice.nextInt();
-    				switch(choice)
+    				do
     				{
-    					case  1 : System.out.println("Sum is : "+stub.sum(getINT(),getINT()));
+    					System.out.println("_________________________Menu___________________________");
+    					System.out.println("1)Sum___________________________________________________");
+    					System.out.println("2)Substraction__________________________________________");
+    					System.out.println("3)Division______________________________________________");
+    					System.out.println("4)Multiplication________________________________________");
+    					System.out.println("5)Factorial_____________________________________________");
+    					System.out.println("6)Power_________________________________________________");
+    					System.out.println("7)Exit__________________________________________________");
+    					System.out.println("________________________________________________________");
+    					System.out.println("Please enter your choice");
+    					choice =scannerChoice.nextInt();
+    					switch(choice)
+    					{
+    						case  1 : System.out.println("Sum is : "+stub.sum(getINT(),getINT()));
     						break;
-    					case  2 : System.out.println("Substraction is : "+stub.substraction(getINT(),getINT()));
+    						case  2 : System.out.println("Substraction is : "+stub.substraction(getINT(),getINT()));
     						break;
-    					case  3 : System.out.println("Division is : "+stub.div(getINT(),getINT()));
+    						case  3 : System.out.println("Division is : "+stub.div(getINT(),getINT()));
     						break;
-    					case  4 : System.out.println("Multiplication is : "+stub.multiplication(getINT(),getINT()));
+    						case  4 : System.out.println("Multiplication is : "+stub.multiplication(getINT(),getINT()));
     						break;
-    					case  5 : System.out.println("Factorial is : "+stub.factorial(getINT()));
+    						case  5 : System.out.println("Factorial is : "+stub.factorial(getINT()));
     						break;
-    					case  6 : System.out.println("Power is : "+stub.power(getINT(),getINT()));
+    						case  6 : System.out.println("Power is : "+stub.power(getINT(),getINT()));
     						break;
-    					case  7 : System.out.println("You quit the application");
+    						case  7 : System.out.println("You quit the application");
     						break;
-    				}
-    			}while((choice<=0) && (choice >7));
-    		}while(choice != 7);
-        }
-        catch (Exception e) 
-        {
-            System.err.println("Client exception: " + e.toString());
-            e.printStackTrace();
-        }
+    					}
+    				}while((choice<=0) && (choice >7));
+			}while(choice != 7);
+		}
+        	catch (Exception e) 
+        	{
+            		System.err.println("Client exception: " + e.toString());
+            		e.printStackTrace();
+		}
 	}
 }
